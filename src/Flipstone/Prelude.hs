362624f91c654807b50f7e69e188f26a4b74680a
Trevis Elser <trevis@flipstone.com>
2021-07-06 14:02:13 -0400

    Add a bunch of "basic" stuff.
    There is still plenty that is very low hanging fruit to add (tuple,
    list, Show, String, and on and on).
    
    For what is here, great care was taken to keep the export ordering the
    same as the import ordering (modulo export grouping with imports from
    multiple modules).
    
    Further, any functions that are mentioned as specializations of others
    are omitted (such as `return`). Exporting and importing all of some
    types and classes could go either way.
    
    Also, note that this does not add any new dependencies or cause any
    omitted functions to be errors, though we may want to do those things
    in the future.

--- src/Flipstone/Prelude.hs
+++ src/Flipstone/Prelude.hs
@@ -1,5 +1,142 @@
 module Flipstone.Prelude
- ( Bool(..)
+ (
+ -- Concrete types
+   Bool(..)
+ , Ordering(..)
+ , Char
+ , Int
+ , Integer
+ , Rational
+ , Int8
+ , Int16
+ , Int32
+ , Int64
+ , Ratio
+ , Word8
+ , Word16
+ , Word32
+ , Word64
+
+ -- Parameterized types
+ -- Maybe and related functions
+ , Maybe(..)
+ , maybe
+ -- Either and related
+ , Either(..)
+ , either
+
+ -- IO and related
+ , IO
+ , liftIO
+
+ -- Typeclasses
+
+ -- Comparison typeclasses and functions
+ , Eq(..)
+ , Ord(..)
+
+ -- Monad related-ish classes and functions
+ , Functor(..)
+ , Applicative(..)
+ , Semigroup(..)
+ , Monoid(mempty, mconcat) -- `mappend` is redundant and not part of the minimal complete definition
+ , Monad((>>=), (>>)) -- `return` is redundant and not part of the minimal complete definition
+ , MonadFail(..)
+ , Arrow(..)
+ , MonadPlus(..)
+ , Alternative(..)
+ , liftA
+ , liftA3
+ , guard
+ , join
+ , void
+ , when
+
+ -- Fold and traversal typeclasses and functions
+ , Traversable(..)
+ , Foldable(foldMap, foldr, foldr1, elem, maximum, sum, product)
+ , null
+ , length
+ , and
+ , or
+ , any
+ , all
+ , concat
+ , concatMap
+ , asum
+ , foldl'
+ , sequenceA_
+ , traverse_
+
+ -- Enumeration typeclasses
+ , Enum(..)
+ , Bounded(..)
+
+ -- Numeric Typeclasses and functions
+ , Num(..)
+ , Real(..)
+ , Integral(..)
+ , Fractional(..)
+ , RealFrac(..)
+ , subtract
+ , even
+ , odd
+ , gcd
+ , lcm
+ , fromIntegral
+ , realToFrac
  ) where
 
-import Prelude (Bool(..))
+import Prelude ( Bool(..)
+               , Ordering(..)
+               , Char
+               , Int
+               , Integer
+               , Rational
+               , Maybe(..)
+               , maybe
+               , Either(..)
+               , either
+               , IO
+               , Eq(..)
+               , Ord(..)
+               , Semigroup(..)
+               , Monoid(mempty, mconcat) -- | `mappend` is redundant and not part of the minimal complete definition
+               , Functor(..)
+               , Applicative(..)
+               , Monad(..)
+               , MonadFail(..)
+               , Traversable(..)
+               , Foldable(foldMap, foldr, foldr1, elem, maximum, sum, product) -- | 'foldl' considered dangerous, use 'foldl\'' instead.
+               , null
+               , length
+               , and
+               , or
+               , any
+               , all
+               , concat
+               , concatMap
+               , Enum(..)
+               , Bounded(..)
+               , Num(..)
+               , Real(..)
+               , Integral(..)
+               , Fractional(..)
+               , RealFrac(..)
+               , subtract
+               , even
+               , odd
+               , gcd
+               , lcm
+               , fromIntegral
+               , realToFrac
+               )
+
+import Control.Applicative (Alternative(..), liftA, liftA3)
+import Control.Arrow (Arrow(..))
+import Control.Monad (MonadPlus(..), guard, join, void, when)
+import Control.Monad.IO.Class (liftIO)
+import Data.Foldable (asum, foldl', sequenceA_, traverse_)
+import Data.Int (Int8, Int16, Int32, Int64)
+import Data.Ratio (Ratio)
+import Data.Word (Word8, Word16, Word32, Word64)
